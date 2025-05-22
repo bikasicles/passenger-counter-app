f95e793a92ab6cfd737711f43bc8d9fb8869794a
bikasicles <liambj007@gmail.com>
2025-05-22 01:14:50 -0600

    nothing special

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// grab the count-el element, store it in a countEl variable
 let countEl = document.getElementById("count-el")
 console.log(countEl)
+
 let count = 0
+console.log(count)
 
 function increment() {
     count = count + 1
-    countEl.innerText = count
     console.log(count)
+    // set countEl's innerText to the count
+    countEl.innerText = count
 }
